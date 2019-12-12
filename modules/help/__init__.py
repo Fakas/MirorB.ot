1492f10cd4a393ed1cf37f8e10b85f2eb1ee564d
Matthew Cazaly <matcaz@bodc.ac.uk>
2019-12-12 14:41:05 +0000

    Added docstrings to module inits

--- modules/help/__init__.py
+++ modules/help/__init__.py
@@ -1,2 +1,4 @@
+"""Help Miror B.ot Module"""
+
 from .help import Help as _Echo
 mb_mods = (_Echo,)
--- modules/radio/__init__.py
+++ modules/radio/__init__.py
@@ -1,2 +1,4 @@
+"""Miror Radio Controller Miror B.ot Module"""
+
 from .radio import RadioController as _RadioController
 mb_mods = (_RadioController,)
