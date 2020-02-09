9c5797af5fcfb02dae674b39a75b57c19a4b2800
Fakas <no_email>
2020-02-09 15:23:32 +0000

    Add games module

---
+++ modules/games/__init__.py
@@ -0,0 +1,4 @@
+"""Game info Miror B.ot module"""
+
+from .games import Games as _Games
+mb_mods = (_Games,)
---
+++ modules/games/games.py
@@ -0,0 +1,43 @@
+"""Game info Miror B.ot module"""
+
+from modules.miror_module import MirorModule
+from modules.asslib import callwebjson
+
+
+class Games(MirorModule):
+    """Game info Miror B.ot module"""
+    mb_mod = True
+    mb_import = True
+    mb_name = "Games"
+
+    mb_default_config = {
+        "api_url": "https://api.serveymcserveface.com/"
+    }
+
+    mb_help = "Games module, displays game info! \n\n" \
+              "Commands: \n" \
+              "__cmd__games - Display game info"
+
+    def __init__(self):
+        self.cfg = self.get_config()
+        self.mb_actions = {
+            "on_command": {
+                "games": self.games_cmd
+            }
+        }
+
+    async def games_cmd(self, *_args, **kwargs):
+        channel = kwargs["channel"]
+        games = callwebjson(self.cfg["api_url"] + "games/")
+        for game_id in games:
+            game = callwebjson(self.cfg["api_url"] + "games/" + game_id)
+            title = game["server_name"] if game["online"] else f"{game_id} (Server offline)"
+            game_name = game["game_name"]
+            map_name = game["map_name"]
+            players = game["player_count"]
+            players_max = game["max_player_count"]
+            info = f"**{title}** \n" \
+                f"> Game: {game_name if game_name is not None else 'N/A'} \n" \
+                f"> Map: {map_name if map_name is not None else 'N/A'} \n" \
+                f"> Players: {f'{players}/{players_max}' if players is not None else 'N/A'} \n"
+            await channel.send(info)
